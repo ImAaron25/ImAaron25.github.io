2fac64f916e40f653b779d92c05a213a4371afea
ImAaron25 <aaronelsisura25@gmail.com>
2026-05-11 11:22:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+// Fade-in effect
+window.onload = function () {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "1s";
+    document.body.style.opacity = 1;
+  }, 100);
+};
