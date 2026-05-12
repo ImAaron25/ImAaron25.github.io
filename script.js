7ac2f2c6d7cb15477a7f7268072ca948425c5dd0
ImAaron25 <aaronelsisura25@gmail.com>
2026-05-12 14:34:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ document.getElementById("searchBar")?.addEventListener("keyup", function () {
     card.style.display = text.includes(filter) ? "block" : "none";
   });
 });
+
+
+
+
+document.querySelectorAll(".accordion-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    let panel = btn.nextElementSibling;
+    panel.style.display = panel.style.display === "block" ? "none" : "block";
+  });
+});
